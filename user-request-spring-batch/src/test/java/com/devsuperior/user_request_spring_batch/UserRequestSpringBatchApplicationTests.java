dd90000916592154eae1d27fed64164279505583
Luis Parente <lgsp25@hotmail.com>
2025-07-15 18:51:39 -0300

    Project created

---
+++ user-request-spring-batch/src/test/java/com/devsuperior/user_request_spring_batch/UserRequestSpringBatchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devsuperior.user_request_spring_batch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserRequestSpringBatchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
